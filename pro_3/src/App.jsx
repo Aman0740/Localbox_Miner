b1f56fb777cf4bf02c2f4a0d8b354b3cdab1afc1
Aman Vagh <157211536+Aman0740@users.noreply.github.com>
2024-05-31 13:09:24 +0000

    update

--- pro_3/src/App.jsx
+++ pro_3/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Navbar from './component/Navbar';
+import Midsection from './component/Midsection';
+import { useContext } from 'react';
+import { Themecontex } from './contex/Themecontex';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const {theme,Togglethem}=useContext(Themecontex)
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  <div>
+  <div style={{backgroundColor:theme==="light"?"white":"black",color:theme==="light"?"black":"white"}}>
+  <Navbar/>
+  <Midsection/>
+  </div>
+  <button onClick={Togglethem}>change</button>
+  </div>
+  );
 }
 
-export default App
+export default App;
--- pro_3/src/main.jsx
+++ pro_3/src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ThemecontexProvider } from './contex/Themecontex.jsx'
+import {AuthcontexProvider} from './contex/AuthContext.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root')).render(
+  <AuthcontexProvider>
+  <ThemecontexProvider>
     <App />
-  </React.StrictMode>,
-)
+  </ThemecontexProvider>
+  </AuthcontexProvider>
+);
